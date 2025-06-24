c90f1e8614f9edfcdb206672f94c2422031c082f
rogereichert <rogereichert@gmail.com>
2025-06-24 01:58:35 -0300

    Delete urls.py

--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import gerar_pdf_dispositivo
-
-urlpatterns = [
-    path('pdf/dispositivo/<int:dispositivo_id>/', gerar_pdf_dispositivo, name="pdf_dispositivo"),
-]
